a97d44474f904984d5ec308d4aa79dfe86fdea30
helloworld <cfinnie3@uwo.ca>
2021-04-02 19:48:34 -0400

    Made Ungrappleable Platforms
    - made ungrappleable platform
    - made ungrappleable crumbling platform
    - changed crumbling to destroy

--- Assets/Scripts/CrumblingPlatform.cs
+++ Assets/Scripts/CrumblingPlatform.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class CrumblingPlatform : MonoBehaviour {
 				[SerializeField] private int delay = 5;
 				[SerializeField] private GameObject self;
-				private IEnumerator wait() {
-								yield return new WaitForSeconds(delay);
-								self.SetActive(false);
-				}
+				
 				private void OnCollisionStay2D(Collision2D collision) {
 								if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Hook") {
-												StartCoroutine(wait());
+												
+												Destroy(self,delay);
+												
 								}
 				}
 }
