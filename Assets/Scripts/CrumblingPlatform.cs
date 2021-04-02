5c56dff2ec6ad3b60b1a7a0e9c2e882775eabb66
helloworld <cfinnie3@uwo.ca>
2021-04-02 19:22:54 -0400

    Crumbling Platform
    -  made crumbling platform
    - tagged the player as player

---
+++ Assets/Scripts/CrumblingPlatform.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrumblingPlatform : MonoBehaviour {
+				[SerializeField] private int delay = 5;
+				[SerializeField] private GameObject self;
+				private IEnumerator wait() {
+								yield return new WaitForSeconds(delay);
+								self.SetActive(false);
+				}
+				private void OnCollisionStay2D(Collision2D collision) {
+								if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Hook") {
+												StartCoroutine(wait());
+								}
+				}
+}
