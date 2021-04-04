02b5fe5030c8d28f543ebdc2043e7a9ee120fcec
Hayman AbdelJabbar <hayman-35@hotmail.com>
2021-04-04 15:36:32 -0400

    Fixed grapple Gun

---
+++ Assets/Gun2.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Gun2 : MonoBehaviour
+{
+    public LayerMask ropeLayerMask;
+
+    public float distance = 90.0f;
+
+    public Transform firePoint;
+
+    LineRenderer line;
+    DistanceJoint2D rope;
+
+    Vector2 lookDirection;
+
+    bool checker = true;
+
+    void Start()
+    {
+        rope = gameObject.AddComponent<DistanceJoint2D>();
+        line = GetComponent<LineRenderer>();
+
+        rope.enabled = false;
+        line.enabled = false;
+    }
+
+    void Update()
+    {
+        line.SetPosition(0, firePoint.position);
+
+        lookDirection = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - transform.position; 
+
+        if (Mouse.current.leftButton.wasPressedThisFrame && checker == true)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, lookDirection, distance, ropeLayerMask);
+
+            if (hit.collider != null)
+            {
+                checker = false;
+                SetRope(hit);
+            }
+        }
+        else if (Mouse.current.leftButton.wasPressedThisFrame && checker == false)
+        {
+            checker = true;
+            DestroyRope();
+        }
+    }
+
+    void SetRope(RaycastHit2D hit)
+    {
+        rope.enabled = true;
+        rope.connectedAnchor = hit.point;
+
+        line.enabled = true;
+        line.SetPosition(1, hit.point);
+    }
+
+    void DestroyRope()
+    {
+        rope.enabled = false;
+        line.enabled = false;
+    }
+}
--- Assets/Scripts/obstacle.cs
+++ Assets/Scripts/obstacle.cs
@@ -1,14 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class obstacle : MonoBehaviour{
-				[SerializeField] private GameObject start;
-				
-				private void OnCollisionEnter2D(Collision2D collision) {
-								if(collision.gameObject.tag=="Player"){
-												collision.gameObject.transform.position = start.transform.position;
-												Physics.SyncTransforms();
-								}
-				}
+public class obstacle : MonoBehaviour
+{
+	[SerializeField] private GameObject start;
+	private void OnCollisionEnter2D(Collision2D collision) {
+	if(collision.gameObject.tag=="Player"){
+		
+		collision.gameObject.transform.position = start.transform.position;
+		Physics.SyncTransforms();
+	}
+	}
 }
