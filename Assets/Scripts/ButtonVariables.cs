092582f48aa8f2a3f86c5547b9176d9433bec59d
Priyank Patel <priyankpatel1308@gmail.com>
2021-04-04 15:45:55 -0400

    Updated button labels

--- Assets/Scripts/ButtonVariables.cs
+++ Assets/Scripts/ButtonVariables.cs
@@ -1,19 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonVariables : MonoBehaviour{
     [SerializeField]private PlatformType type;
     [SerializeField]private int quantity;
     [SerializeField]private PlatformSelectUI selector;
+
+
+	public Text textfield;
+
+
     private int used=0;
+
+	void Start(){
+		textfield.text = "Remaining: " + quantity.ToString();
+	}
+
     
-				public void OnButtonPress() {
+	public void OnButtonPress() {
         if (used < quantity) {
             used++;
+			int count = quantity - used;
+			textfield.text = "Remaining: " + count.ToString();
             selector.createGhost(type);
-            
-        }
+       }
         
 				}
 }
