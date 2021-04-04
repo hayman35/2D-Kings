5d1299a1aede639e7ab0978ef12ceac1592d8b6c
helloworld <cfinnie3@uwo.ca>
2021-04-04 15:12:55 -0400

    Obstacles Updated to reset placed platforms

--- Assets/Scripts/obstacle.cs
+++ Assets/Scripts/obstacle.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class obstacle : MonoBehaviour{
 				[SerializeField] private GameObject start;
+				[SerializeField] private ItemBuilder builder;
 				
 				private void OnCollisionEnter2D(Collision2D collision) {
 								if(collision.gameObject.tag=="Player"){
+												builder.resetPlatforms();
 												collision.gameObject.transform.position = start.transform.position;
 												Physics.SyncTransforms();
 								}
