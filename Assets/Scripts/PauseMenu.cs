3bda84159609acf8aaf5f32c450bdd849df30822
PengJu Chang <pchang32@uwo.ca>
2021-04-05 19:02:07 -0400

    Added UI to all 10 Levels

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour{
+				[SerializeField] private int mainMenuID;
+				[SerializeField] private PlayerController player;
+				[SerializeField] private GameObject help;
+				private bool helpBool = false;
+
+				public void resume() {
+								player.pause();
+				}
+				public void mainMenu() {
+								SceneManager.LoadScene(mainMenuID);
+				}
+				public void quitGame() {
+								Application.Quit();
+				}
+				public void showHelp() {
+								if (!helpBool) {
+												help.SetActive(true);
+												helpBool = true;
+								}
+								else {
+												help.SetActive(false);
+												helpBool = false;
+								}
+				}
+}
