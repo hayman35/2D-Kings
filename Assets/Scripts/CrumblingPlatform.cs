1c37ca394bb5d1d93f60c4e04a4851d3fde93f7e
Hayman AbdelJabbar <hayman-35@hotmail.com>
2021-04-06 19:19:40 -0400

    new platform

--- Assets/Scripts/CrumblingPlatform.cs
+++ Assets/Scripts/CrumblingPlatform.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CrumblingPlatform : MonoBehaviour {
-	[SerializeField] private int delay = 5;
-	[SerializeField] private GameObject self;
-		
+	[SerializeField] private float fallDelay = 1f;
+	Rigidbody2D rigidbody;
+
+	private void Awake() 
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
 	private void OnCollisionEnter2D(Collision2D collision) 
 	{
-		if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Hook") 
+		if (collision.gameObject.tag == "Player") 
 		{	
-			Destroy(self,delay);								
+			Invoke("DropPlatform",fallDelay);
+			Destroy(gameObject, 2f);							
 		}
 	}
+	
+
+	void DropPlatform()
+	{
+		rigidbody.isKinematic = false;
+	}
+
 }
